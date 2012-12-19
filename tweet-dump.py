0a91ab623732bd4600548a9160f3289f810210a5
Pat Hawks <pat@pathawks.com>
2012-12-18 21:05:39 -0400

    Update tweet-dump.py

--- tweet-dump.py
+++ tweet-dump.py
@@ -18,9 +18,11 @@ __author__ = 'Casey Dunham <casey.dunham@gmail.com>'
 __version__ = '0.1'
 
 import argparse
-import urllib
+import codecs
+import datetime
 import sys
 import time
+import urllib
 
 from urllib2 import (Request, urlopen, HTTPError, URLError)
 
@@ -48,15 +50,36 @@ class TweetDumpError(Exception):
 class RateLimitError(TweetDumpError):
     pass
 
-API_URL = "https://api.twitter.com/1/statuses/user_timeline.json?%s"
+BASE_API_URL = "https://api.twitter.com/1/"
+API_URL  = BASE_API_URL + "statuses/user_timeline.json?%s"
+PIC_URL = BASE_API_URL + "users/profile_image/{screen_name}.png?size={size}"
+USER_URL = BASE_API_URL + "users/show.json?%s"
 
 # we are not authenticating so this will return the rate limit based on our IP
 # see (https://dev.twitter.com/docs/api/1/get/account/rate_limit_status)
-RATE_LIMIT_API_URL = "https://api.twitter.com/1/account/rate_limit_status.json"
+RATE_LIMIT_API_URL = BASE_API_URL + "account/rate_limit_status.json"
 
 parser = argparse.ArgumentParser(description="dump all tweets from user")
 parser.add_argument("handle", type=str, help="twitter screen name")
 
+
+def get_user( screen_name ):
+    params = {
+        "screen_name": screen_name
+    }
+
+    encoded_params = urllib.urlencode( params )
+    query = USER_URL % encoded_params
+    resp = fetch_url(query)
+    twitter_user = json.loads(resp.read())
+    return twitter_user
+
+def base64_image( image_url ):
+    return "data:image/png;base64," + urllib.urlopen( image_url ).read().encode( "base64" ).replace( "\n", '' ).replace( "\r", '' )
+
+def get_user_profile_pic( screen_name, size ):
+    query = PIC_URL.format( screen_name=screen_name, size=size )
+    return base64_image( query )
 
 def get_tweets(screen_name, count, maxid=None):
     params = {
@@ -137,14 +160,65 @@ if __name__ == '__main__':
     requests_made = 0
     tweet_count = 0
 
+    twitter_user = get_user(screen_name)
+
+    out_file.write('<!DOCTYPE html>\n<html lang="en">\n\t<head>\n\t\t<meta charset="utf-8">\n')
+    out_file.write('\t\t<!--\n\t\t\t{title}\n\t\t\t{url}\n\t\t\t{date}\n\t\t-->\n'.format( title='tweet-dump.py',url='https://github.com/pathawks/tweet-dump', date='Backup Date: '+datetime.date.today().strftime("%B %d, %Y") ) )
+    out_file.write('\t\t<title>{name} ({screen_name}) - Tweet Dump</title>\n'.format( name=twitter_user["name"], screen_name=twitter_user["screen_name"] ) )
+    out_file.write('\t\t<base href="http://twitter.com/">\n' )
+    out_file.write('\t\t<link rel="icon" href="{url}" type="image/png"/>\n'.format( url=twitter_user["profile_image_url"] ) )
+    out_file.write('\t\t<style>\n')
+    out_file.write('\t\t\tbody{')
+    out_file.write('text-align:center;color:#{color};padding:0;margin:0;font-family:"Helvetica Neue", Arial, sans-serif;font-size:14px;'.format( color=twitter_user["profile_text_color"] ) )
+    
+    if twitter_user["profile_use_background_image"]:
+        if twitter_user["profile_background_tile"]:
+            out_file.write('background:#{bg} url("{url}") repeat top left;'.format( bg=twitter_user["profile_background_color"], url=base64_image( twitter_user["profile_background_image_url"] ) ) )
+        else:
+            out_file.write('background:#{bg} url("{url}") no-repeat top left;'.format( bg=twitter_user["profile_background_color"], url=base64_image( twitter_user["profile_background_image_url"] ) ) )
+    else:
+        out_file.write('background:#{bg};'.format( bg=twitter_user["profile_background_color"] ) )
+    
+    out_file.write('background-attachment:fixed}\n')
+    out_file.write('\t\t\ta{')
+    out_file.write('color:#{color}'.format( color=twitter_user["profile_link_color"] ) )
+    out_file.write('}\n')
+    out_file.write('\t\t\t#container{')
+    out_file.write('margin:0 auto;width:500px;background:rgba(255,255,255,0.3);padding:8px 14px' )
+    out_file.write('}\n')
+    out_file.write('\t\t\t#profile,blockquote{background:#fff;border:#DDD 1px solid;border-radius:5px;width:480px;margin:0 0 10px;padding:0 0 20px 20px;text-align:left}\n')
+    out_file.write('\t\t\t#profile{color:#fff;background:#ccc;background:rgba(0,0,0,0.6);width:490px;text-align:center;padding:5px}\n')
+    out_file.write('\t\t\t.profile-picture{margin:20px auto 6px;border:#fff 4px solid;border-radius:4px;display:block;width:73px;height:73px}\n')
+    out_file.write('\t\t\t.profile-picture .avatar{border-radius:3px}\n')
+    out_file.write('\t\t\t.profile-card-inner h1{font-size:24px;text-shadow:rgba(0, 0, 0, 0.5) 0px 0.6px 0.6px}\n')
+    out_file.write('\t\t\t.profile-card-inner h2{font-size:18px;font-weight:normal}\n')
+    out_file.write('\t\t</style>\n')
+    out_file.write('\t</head>\n\t<body>\n')
+    out_file.write('\t\t<div id="container">')
+
+    out_file.write('\n\t\t<div id="profile">')
+    out_file.write('\n\t\t\t<a href="{avatar}" class="profile-picture">'.format( avatar=get_user_profile_pic( screen_name, 'original' ) ) )
+    out_file.write('\n\t\t\t\t<img src="{avatar}" alt="{name}" class="avatar">'.format( avatar=get_user_profile_pic( screen_name, 'bigger' ), name=twitter_user["name"] ) )
+    out_file.write('\n\t\t\t</a>')
+    out_file.write('\n\t\t\t<div class="profile-card-inner">')
+    out_file.write('\n\t\t\t<h1 class="fullname">{name}</h1>'.format( name=twitter_user["name"] ) )
+    out_file.write('\n\t\t\t<h2 class="username"><span class="screen-name">@{screen_name}</span></h2>'.format( screen_name=twitter_user["screen_name"] ) )
+    out_file.write('\n\t\t\t<p class="bio ">{bio}</p>'.format( bio=twitter_user["description"] ) )
+    out_file.write('\n\t\t\t<p class="location-and-url">')
+    out_file.write('\n\t\t\t\t<span class="location">{location}</span>'.format( location=twitter_user["location"] ) )
+    out_file.write('\n\t\t\t\t<span class="url">')
+    
+    if twitter_user["url"]:
+        out_file.write('\n\t\t\t\t\t<a href="{url}">{url}</a>'.format( url=twitter_user["url"] ) )
+    
+    out_file.write('\n\t\t\t\t</span>')
+    out_file.write('\n\t\t\t</p>')
+    out_file.write('\n\t\t\t</div>')
+    out_file.write('\n\t\t</div>\n')
+
     while True:
         # get initial rate information
         (remaining, rst_time_s, rst_time) = get_initial_rate_info()
-        
-        out_file.write('<!DOCTYPE html>\n<html lang="en">\n\t<head>\n\t\t<meta charset="utf-8">\n')
-        out_file.write('\t\t<title>{screen_name} Tweet Dump</title>\n'.format( screen_name = screen_name ) )
-        out_file.write('\t\t<!--\n\t\t\t{title}\n\t\t\t\t{url}\n\t\t-->\n'.format( title='tweet-dump.py',url='https://github.com/pathawks/tweet-dump' ) )
-        out_file.write('\t</head>\n\t<body>\n')
 
         while remaining > 0:
             try:
@@ -161,14 +235,16 @@ if __name__ == '__main__':
                         date_data = time.strftime( "%Y-%m-%dT%H:%M:%S+00:00", date_data )
                         maxid = tweet["id"]
                         tweet_count += 1
-                        out_file.write('\t\t<blockquote class="twitter-tweet"><p>{tweet_text}</p>&mdash; {user_name} (@{screen_name}) <a href="https://twitter.com/{user_id}/status/{id}" data-datetime="{date_data}">{date_string}</a></blockquote>\n'.format( tweet_text=repr(tweet["text"]), id=tweet["id"], screen_name=tweet["user"]["screen_name"], user_name=tweet["user"]["name"], user_id=tweet["user"]["id"], date_string=tweet["created_at"], date_data=date_data ) )
+                        tweet["text"] = tweet["text"].encode('ascii', 'ignore')
+                        out_file.write('\t\t\t<blockquote class="twitter-tweet tw-align-center" width="500"><p>{tweet_text}</p>&mdash; {user_name} (@{screen_name}) <a href="https://twitter.com/{user_id}/status/{id}" data-datetime="{date_data}">{date_string}</a></blockquote>\n'.format( tweet_text=tweet["text"], id=tweet["id"], screen_name=tweet["user"]["screen_name"], user_name=tweet["user"]["name"], user_id=tweet["user"]["id"], date_string=tweet["created_at"], date_data=date_data ) )
                 else:
                     print "[*] reached end of tweets"
                     break
 
         break
 
-        out_file.write('\t\t<script src="http://platform.twitter.com/widgets.js" charset="utf-8"></script>\n')
-        out_file.write('\t</body>\n</html>')
+    out_file.write('\t\t</div>\n')
+    out_file.write('\t\t<script src="http://platform.twitter.com/widgets.js" charset="utf-8"></script>\n')
+    out_file.write('\t</body>\n</html>')
 
     print "[*] %d tweets dumped!" % tweet_count
