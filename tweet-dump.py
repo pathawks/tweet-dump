e8e357b8b76c70a850344b8bed0fa2f7192c5894
Pat Hawks <pat@pathawks.com>
2013-08-07 19:31:15 -0400

    Fix tabs

--- tweet-dump.py
+++ tweet-dump.py
@@ -68,70 +68,70 @@ HTML_TEMPLATE = """
 """
 
 def print_banner():
-  print "tweet-dump %s ©2013 %s" % (__version__, __author__)
-  print """     .-.
     (. .)__,')
     / V      )
     \  (   \/ .
      `._`.__\\ o ,
         <<  `'   .o..
+	print "tweet-dump %s ©2013 %s" % (__version__, __author__)
+	print """     .-.
 	"""
 
 def Usage():
-  print_banner()
-  print 'Usage: %s [options] twitterid' % __file__
-  print
-  print '  This script fetches a users latest twitter update and stores'
-  print '  the result in a file as an XHTML fragment'
-  print
-  print '  Options:'
-  print '    --help -h : print this help'
-  print '    --output : the output file [default: stdout]'
+	print_banner()
+	print 'Usage: %s [options] twitterid' % __file__
+	print
+	print '  This script fetches a users latest twitter update and stores'
+	print '  the result in a file as an XHTML fragment'
+	print
+	print '  Options:'
+	print '    --help -h : print this help'
+	print '    --output : the output file [default: stdout]'
 
 
 def FetchTwitter(user, output):
-  assert user
-  statuses = twitter.Api().GetUserTimeline(id=user, count=1)
-  s = statuses[0]
-  xhtml = TWEET_TEMPLATE.format(
+	assert user
+	statuses = twitter.Api().GetUserTimeline(id=user, count=1)
+	s = statuses[0]
+	xhtml = TWEET_TEMPLATE.format(
 		tweet_text = s.text,
 		user_name = s.user.name,
 		screen_name = s.user.screen_name,
 		id = s.id,
 		date_datetime = s.created_at_in_seconds,
 		date_string = s.relative_created_at
-  )
-  if output:
-    Save(xhtml, output)
-  else:
-    print xhtml
+	)
+	if output:
+		Save(xhtml, output)
+	else:
+		print xhtml
 
 
 def Save(xhtml, output):
-  out = codecs.open(output, mode='w', encoding='ascii',
-                    errors='xmlcharrefreplace')
-  out.write(xhtml)
-  out.close()
+	out = codecs.open(output, mode='w', encoding='ascii',
+			              errors='xmlcharrefreplace')
+	out.write(xhtml)
+	out.close()
 
 def main():
-  try:
-    opts, args = getopt.gnu_getopt(sys.argv[1:], 'ho', ['help', 'output='])
-  except getopt.GetoptError:
-    Usage()
-    sys.exit(2)
-  try:
-    user = args[0]
-  except:
-    Usage()
-    sys.exit(2)
-  output = None
-  for o, a in opts:
-    if o in ("-h", "--help"):
-      Usage()
-      sys.exit(2)
-    if o in ("-o", "--output"):
-      output = a
-  FetchTwitter(user, output)
+	try:
+		opts, args = getopt.gnu_getopt(sys.argv[1:], 'ho', ['help', 'output='])
+	except getopt.GetoptError:
+		Usage()
+		sys.exit(2)
+	try:
+		user = args[0]
+	except:
+		Usage()
+		sys.exit(2)
+	output = None
+	for o, a in opts:
+		if o in ("-h", "--help"):
+			Usage()
+			sys.exit(2)
+		if o in ("-o", "--output"):
+			output = a
+	FetchTwitter(user, output)
 
 if __name__ == "__main__":
-  main()
+	main()
