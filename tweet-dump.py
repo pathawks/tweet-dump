a9bae919d0817e1c31af45d9fb619fd78f764ea3
Pat Hawks <pat@pathawks.com>
2013-08-07 16:48:39 -0400

    We don't need this blank line here.

--- tweet-dump.py
+++ tweet-dump.py
@@ -76,7 +76,6 @@ def Usage():
         <<  `'   .o..
 
     """
-
   print 'Usage: %s [options] twitterid' % __file__
   print
   print '  This script fetches a users latest twitter update and stores'
